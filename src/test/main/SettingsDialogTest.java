ea2078468eb295c219830765f9f97c914aa67d33
MonicaAlessi <62215499+MonicaAlessi@users.noreply.github.com>
2020-09-04 16:50:16 +0200

    update tests

--- src/test/main/SettingsDialogTest.java
+++ src/test/main/SettingsDialogTest.java
@@ -3,15 +3,18 @@ package test.main;
 import javax.swing.JFrame;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import dialogs.SettingsDialog;
+import main.Application;
 
 public class SettingsDialogTest {
- 
-    JFrame frametest2 = new JFrame();
-    SettingsDialog settingsDialogTest = new SettingsDialog(frametest2, null, false);
- 
-    @Test
+
+    Application applicationtest = new Application();
+    JFrame frametest2 =  Application.getAppWindow();
+    SettingsDialog settingsDialogTest = new SettingsDialog(frametest2, "test", true);
+    
+    @Test //tbd
     public void testCountComponentStyleSettingPanek(){
         Assert.assertEquals(2,settingsDialogTest.getStyleSettingsPanel().getComponentCount());
     }
