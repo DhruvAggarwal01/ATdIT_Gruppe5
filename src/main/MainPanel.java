a98b59ed67a86a7b5f92d0f254d37a83cd5fba2d
maikfichtenkamm <62957774+maikfichtenkamm@users.noreply.github.com>
2020-09-07 19:06:28 +0200

    Update MainPanel.java

--- src/main/MainPanel.java
+++ src/main/MainPanel.java
@@ -1,8 +1,10 @@
 package main;
 
+
 import java.awt.*;
 
 import javax.swing.*;
+
 
 /**
  * Diese Klasse leitet den Aufbau der Anwendung ein.
@@ -11,14 +13,14 @@ import javax.swing.*;
  *         Lahr
  */
 public class MainPanel extends JFrame {
-
+ 
     private static final long serialVersionUID = -8417942669407317542L;
-
+ 
     private static String appTitle = "Steinbruch ALBERSWEILER";
     private static NavigationPane navPane;
     private static HeaderPanel headerPanel;
     private Container c;
-
+ 
     /**
      * Konstruktor, der zuständig für den Aufbau des Hauptframe-Fensters ist. Das
      * Fenster setzt sich aus Header, Navigationsleiste und dem dazugehörigen
@@ -28,13 +30,16 @@ public class MainPanel extends JFrame {
         c = getContentPane();
         c.setLayout(new BorderLayout());
 
+
         headerPanel = new HeaderPanel("<html><p>" + appTitle + "</p></html>");
         navPane = new NavigationPane(JTabbedPane.LEFT, JTabbedPane.SCROLL_TAB_LAYOUT);
 
+
+ 
         c.add(headerPanel, BorderLayout.NORTH);
         c.add(navPane, BorderLayout.CENTER);
     }
-
+ 
     /**
      * Getter-Methode für <code>appTitle</code>
      * 
@@ -43,7 +48,7 @@ public class MainPanel extends JFrame {
     public static String getAppTitle() {
         return appTitle;
     }
-
+ 
     /**
      * Getter-Methode für <code>navPane</code>
      * 
@@ -52,7 +57,7 @@ public class MainPanel extends JFrame {
     public static NavigationPane getNavPane() {
         return navPane;
     }
-
+ 
     /**
      * Getter-Methode für <code>headerPanel</code>
      * 
@@ -61,21 +66,21 @@ public class MainPanel extends JFrame {
     public static HeaderPanel getHeaderPanel() {
         return headerPanel;
     }
-
+ 
     /**
      * Setter-Methode für <code>appTitle</code>
      */
     public static void setAppTitle(String appTitleSet) {
         appTitle = appTitleSet;
     }
-
+ 
     /**
      * Setter-Methode für <code>navPane</code>
      */
     public static void setNavPane(NavigationPane navPaneSet) {
         navPane = navPaneSet;
     }
-
+ 
     /**
      * Setter-Methode für <code>headerPanel</code>
      */
