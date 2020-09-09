05fbab79d5bff22536cac1bb2e55aad15e1f185b
Sophie Orth <sophie.orth@sap.com>
2020-09-09 23:03:09 +0200

    update db orders

--- src/db_interaction/Order.java
+++ src/db_interaction/Order.java
@@ -1,21 +1,20 @@
 package db_interaction;
 public class Order {
     
-
     public static int order_id = 0;
     public static String firm = "";
     public static String stone_type = ""; 
     public static String due_date = "";
-    public static String status = "";
+    public static String phase = "";
     public static int amount = 00000;
     public static int Price = 0;
-    public static boolean done = false; // immer syncen mit Excel-DB (im Hinblick auf ausloggen)
-
+    public static boolean done = false; 
+    public static String status = "";
 
     @Override
     public String toString() {
         return "{" + "order_id: " + order_id + " ; firm: " + firm + " ; stone_type: " + stone_type
-                + " ; due_date: " + due_date + " ; status: " + status + " ; amount: " + amount + " ; Price: " + Price
-                + " ; done: " + done + "}";
+                + " ; due_date: " + due_date + " ; phase: " + phase + " ; amount: " + amount + " ; Price: " + Price
+                + " ; done: " + done + "  ; status: " + status + "}";
     }
 }
